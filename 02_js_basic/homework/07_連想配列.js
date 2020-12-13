e6308f4f2ab74693a621c0bda8b69fa50b55e048
ShiotaYoshiaki <shiota.yosiaki.0904@gmail.com>
2020-12-14 01:55:13 +0900

    add question

--- 02_js_basic/homework/07_連想配列.js
+++ 02_js_basic/homework/07_連想配列.js
@@ -82,8 +82,56 @@ const test3 = [
   },
   {
     name: 'test4',
-    value: '4',
+    value: '2',
   },
 ];
 console.log();
 
+/**
+ * 問5. 連想配列test5内にある
+ * valueが変数getNumが含まれるobjectを抽出し、
+ * consoleにて表示してください
+ * また、findを使用してください
+ */
+const getNum =  '1';
+const test3 = [
+  {
+    name: 'test1',
+    value: '1',
+  },
+  {
+    name: 'test2',
+    value: '2',
+  },
+  {
+    name: 'test3',
+    value: '3',
+  },
+  {
+    name: 'test4',
+    value: '2',
+  },
+];
+console.log();
+
+/**
+ * 問6. findとfilterの違いを答えてください
+ */
+
+/**
+ * 問6. 回答欄
+ * 
+ * 
+ * 
+ */
+
+/**
+ * 問7. mapとfilterの違いを答えてください
+ */
+
+/**
+ * 問6. 回答欄
+ * 
+ * 
+ * 
+ */
