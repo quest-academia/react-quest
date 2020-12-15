5c5b17826aaf0ccc3770877c3a4cde75e4485d7e
sedoriProg <sedori.prog@gmail.com>
2020-12-15 15:14:56 +0900

    真偽問題回答

--- 02_js_basic/homework/04_真偽.js
+++ 02_js_basic/homework/04_真偽.js
@@ -1,26 +1,37 @@
 /**
  * 出力結果がどうなるか予想しながら、consoleを表示してください
  */
+
+// truthy
 const string = 'string';
-// console.log('string: ' + !!string);
+console.log('string: ' + !!string);
 
+// truthy
 const num = 6;
-// console.log('num: ' + !!num);
+console.log('num: ' + !!num);
 
+// falsy
 const zero = 0;
-// console.log('zero: ' + !!zero);
+console.log('zero: ' + !!zero);
 
+// falsy
 const emptyString = '';
-// console.log('emptyString: ' + !!emptyString);
+console.log('emptyString: ' + !!emptyString);
 
+// falsy
 const nullParam = null;
-// console.log('nullParam: ' + !!nullParam);
+console.log('nullParam: ' + !!nullParam);
 
+// falsy
 const undefinedParam = undefined;
-// console.log('undefinedParam: ' + !!undefinedParam);
+console.log('undefinedParam: ' + !!undefinedParam);
 
+// truthy(オブジェクトが"ある"っていう判定)
+//「空配列はfalse」という判定がしたい場合はObject.keys()とlength使う
 const emptyObject = {};
-// console.log('emptyObject: ' + !!emptyObject);
+console.log('emptyObject: ' + !!emptyObject);
 
+// truthy(配列が"ある"っていう判定。
+// 「空配列はfalse」という判定がしたい場合はlength使う
 const emptyArray = [];
-// console.log('emptyArray: ' + !!emptyArray);
+console.log('emptyArray: ' + !!emptyArray);
