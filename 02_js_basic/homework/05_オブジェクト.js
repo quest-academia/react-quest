bd9b96e8673872da573c1d3bf89c14fffa31fb16
Nakazawa Natsumi <natsumin83@icloud.com>
2020-12-19 16:44:38 +0900

    第二回宿題JS

--- 02_js_basic/homework/05_オブジェクト.js
+++ 02_js_basic/homework/05_オブジェクト.js
@@ -7,7 +7,7 @@ const test1 = {
     prop: ["target"],
   },
 };
-console.log();
+console.log(test1.obj.prop[0]);
 
 /**
  * 問2. test2からobjectのプロパティを配列で取得してください
@@ -16,7 +16,7 @@ const test2 = {
   sample: "test",
   dummy: "value",
 };
-console.log();
+console.log(Object.getOwnPropertyNames(test2));
 
 /**
  * 問3. test3からtargetをconsoleで表示してください
@@ -33,20 +33,26 @@ const test3 = {
     },
   },
 };
-console.log();
+console.log(test3.a.b.c.d.target);
 
 /**
  * 問4. あなたのプロフィールをオブジェクトで表現してください
  */
 
-const yourPro = {};
+const yourPro = {
+  name: "中澤夏実",
+  address: "Tokyo",
+  age: 26,
+};
 console.log(yourPro);
 
 /**
  * 問5. test5オブジェクトのsampleに紐づく値'test'の
  * 値を'react-quest'に変更してください
  */
-const test5 = {
+
+let test5 = {
   sample: "test",
 };
-console.log();
+test5.sample = "react-quest";
+console.log(test5);
--- 02_js_basic/homework/07_連想配列.js
+++ 02_js_basic/homework/07_連想配列.js
@@ -4,11 +4,11 @@
  */
 const test1 = [
   {
-    name: 'target',
-    type: 'test',
+    name: "target",
+    type: "test",
   },
 ];
-console.log();
+console.log(test1[0].name);
 
 /**
  * 問2. 連想配列test2から
@@ -16,23 +16,23 @@ console.log();
  */
 const test2 = [
   {
-    name: 'test1',
-    value: '1',
+    name: "test1",
+    value: "1",
   },
   {
-    name: 'test2',
-    value: '2',
+    name: "test2",
+    value: "2",
   },
   {
-    name: 'test3',
-    value: '3',
+    name: "test3",
+    value: "3",
   },
   {
-    name: 'test4',
-    value: '4',
+    name: "test4",
+    value: "4",
   },
 ];
-console.log();
+console.log(test2[2]);
 
 /**
  * 問3. 連想配列test3内にある
@@ -40,25 +40,37 @@ console.log();
  * 新しい連想配列を作成してください。
  * 作成した連想配列はconsoleで表示をしてください
  */
+
+const plus = 4;
 const test3 = [
   {
-    name: 'test1',
-    value: '1',
+    name: "test1",
+    value: "1",
   },
   {
-    name: 'test2',
-    value: '2',
+    name: "test2",
+    value: "2",
   },
   {
-    name: 'test3',
-    value: '3',
+    name: "test3",
+    value: "3",
   },
   {
-    name: 'test4',
-    value: '4',
+    name: "test4",
+    value: "4",
   },
 ];
-console.log();
+
+// 最初はこの書き方をしましたが、mapメソッドを使うべきなのか迷い、mapメソッドを残しました。
+// let newArray = [];
+// test3.forEach((element) => {
+//   newArray.push({ name: element.name, value: Number(element.value) + plus });
+// });
+
+const newArray = test3.map((element) => Number(element.value) + plus);
+newArray.forEach((element) => {
+  console.log(element);
+});
 
 /**
  * 問4. 連想配列test4内にある
@@ -66,26 +78,35 @@ console.log();
  * consoleにて表示してください
  * また、filterを使用してください
  */
-const getNum =  '2';
-const test3 = [
+const getNum = "2";
+const test4 = [
   {
-    name: 'test1',
-    value: '1',
+    name: "test1",
+    value: "1",
   },
   {
-    name: 'test2',
-    value: '2',
+    name: "test2",
+    value: "2",
   },
   {
-    name: 'test3',
-    value: '3',
+    name: "test3",
+    value: "3",
   },
   {
-    name: 'test4',
-    value: '2',
+    name: "test4",
+    value: "2",
   },
 ];
-console.log();
+
+const filterdValue = test4.filter((x) => {
+  if (x.value === getNum) {
+    return x;
+  }
+});
+
+filterdValue.forEach((element) => {
+  console.log(element);
+});
 
 /**
  * 問5. 連想配列test5内にある
@@ -93,26 +114,29 @@ console.log();
  * consoleにて表示してください
  * また、findを使用してください
  */
-const getNum =  '1';
-const test3 = [
+const getNum02 = "1";
+const test5 = [
   {
-    name: 'test1',
-    value: '1',
+    name: "test1",
+    value: "1",
   },
   {
-    name: 'test2',
-    value: '2',
+    name: "test2",
+    value: "2",
   },
   {
-    name: 'test3',
-    value: '3',
+    name: "test3",
+    value: "3",
   },
   {
-    name: 'test4',
-    value: '2',
+    name: "test4",
+    value: "2",
   },
 ];
-console.log();
+
+const foundValue = test5.find((element) => element.value === getNum02);
+
+console.log(foundValue);
 
 /**
  * 問6. findとfilterの違いを答えてください
@@ -120,18 +144,19 @@ console.log();
 
 /**
  * 問6. 回答欄
- * 
- * 
- * 
+ * findメソッドは参照した配列から最初に条件に合った要素の値を返すので、値は配列の要素の値である。
+ * filterメソッドは参照した配列から、条件に合ったものから新しい配列を生成するので、値は配列である。
+ *
  */
 
 /**
  * 問7. mapとfilterの違いを答えてください
+ *
  */
 
 /**
  * 問6. 回答欄
- * 
- * 
- * 
+ * mapメソッドは参照した配列の要素の値に対して、変更を加えて新しい配列を生成する。
+ * filterメソッドは参照した配列から、条件に合ったものから新しい配列を生成する。
+ *
  */
