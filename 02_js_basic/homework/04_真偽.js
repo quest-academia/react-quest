889268ebea3187872b87cd0b1dd520df359af429
shuho <showing838@gmail.com>
2020-12-24 01:51:53 +0900

    真偽課題

--- 02_js_basic/homework/04_真偽.js
+++ 02_js_basic/homework/04_真偽.js
@@ -1,26 +1,28 @@
 /**
  * 出力結果がどうなるか予想しながら、consoleを表示してください
  */
-const string = 'string';
+const string = "string";
 // console.log('string: ' + !!string);
-
+// true
 const num = 6;
 // console.log('num: ' + !!num);
-
+// true
 const zero = 0;
 // console.log('zero: ' + !!zero);
-
-const emptyString = '';
-// console.log('emptyString: ' + !!emptyString);
-
+// false
+const emptyString = "";
+// console.log("emptyString: " + !!emptyString);
+// false
 const nullParam = null;
 // console.log('nullParam: ' + !!nullParam);
-
+// false
 const undefinedParam = undefined;
 // console.log('undefinedParam: ' + !!undefinedParam);
-
+// false
 const emptyObject = {};
 // console.log('emptyObject: ' + !!emptyObject);
-
+// false ×
+// true
 const emptyArray = [];
 // console.log('emptyArray: ' + !!emptyArray);
+// true
