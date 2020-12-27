6787bb82d9e5373571d9639bce4921ea331d0850
福田辰彦 <fukudatatsuhiko@fukudatatsuhikonoMacBook-Pro.local>
2020-12-27 11:56:15 +0900

    add:04_クラス.js回答

--- 03_js_basic/homework/04_クラス.js
+++ 03_js_basic/homework/04_クラス.js
@@ -4,12 +4,10 @@
 
 /**
  * 問1. 回答欄
- * 
- * 
- * 
+ * 色々な関数をひとまとめにして、使い回せるようにした
+ * オブジェクトの集合体
+ * newを使うことでクラスを使用できる
  */
-
-
 
 /**
  * 問2. newとは何か説明してください
@@ -17,21 +15,23 @@
 
 /**
  * 問2. 回答欄
- * 
- * 
+ * newを宣言することでクラスを使用することができる
+ * オブジェクト型のインスタンスを作成する
  * 
  */
-
-
 
 /**
  * 問3. Personクラスを変数に格納し、文字列yourNameをクラスから呼び出してconsoleから表示してください
  */
-class Person {
-  name = 'yourName';
-}
+// class Person {
+//     name = 'yourName';
+// }
 
+// const targetClass = new Person();
+// console.log(targetClass.name);
 
+// 問3. 回答欄
+// yourName
 
 /**
  * 問4. thisとは何か、説明して下さい
@@ -41,11 +41,11 @@ class Person {
 /**
  * 問4. 回答欄
  * 
- * 
+ * 自分の所属しているオブジェクト(クラス内)
+ * クラス内の値を呼び出すときに使用する
+ * console.log(this.呼び出したい値)
  * 
  */
-
-
 
 /**
  * 問5. constructorとは何か、説明してください
@@ -54,12 +54,11 @@ class Person {
 
 /**
  * 問5. 回答欄
- * 
- * 
+ * クラスで作成されたオブジェクトの生成と初期化のための特殊なメソッド
+ * クラスをインスタンス化するときに使用
+ * thisと関係している？
  * 
  */
-
-
 
 /**
  * 問6. 下記を実行してください
@@ -69,5 +68,18 @@ class Person {
  * 4. consoleにてconstructorの中にある'test param'を呼び出してください
  */
 
- 
- 
+// class Test6 {
+//     constructor(hoge, foo) {
+//         this.hoge = 'test param'
+//         this.foo = 'test1'
+//     }
+// };
+
+// class Test6 = {
+//     testFanc() {
+//         console.log();
+//     };
+//     // param = 'test param';
+// };
+
+// const Test6 = new test6();
