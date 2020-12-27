fe0a5d1da563b6b9ab3f57ef17f74b004e477e40
Nakazawa Natsumi <natsumin83@icloud.com>
2020-12-27 21:17:52 +0900

    react 作成

---
+++ 04_react_component/class/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const test = 'test Param';
+  return (
+    <div className="App">
+      <header className="App-header">{test}</header>
+    </div>
+  );
+}
+
+export default App;
