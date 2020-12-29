2f03a4948296456c31b242b63bcfdcfb352fd1a4
Nakazawa Natsumi <natsumin83@icloud.com>
2020-12-29 23:14:28 +0900

    練習問題04

--- 04_react_component/class/src/App.js
+++ 04_react_component/class/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Work01 from './components/training/Work01';
 import BigButton from './components/training/BigButton';
 import FlexibleButton from './components/training/FlexibleButton';
+import DifficultButton from './components/training/DifficultButton';
 
 function App() {
   // 問題01
@@ -30,12 +31,45 @@ function App() {
     flexibleButtonArray.push(<FlexibleButton buttonWidth={i * 20 + 100} />);
   }
 
+  // 問題04
+  const difficultButtonArray = [];
+
+  for (let i = 1; i <= 13; i++) {
+    function difficultButtonColor() {
+      // if (i % 3 === 0) {
+      //   return 'red';
+      // } else {
+      //   return 'blue';
+      // }
+
+      return i % 3 === 0 ? 'red' : 'blue';
+    }
+    function difficultButtonText(i) {
+      // if (i % 4 === 0) {
+      //   return i + '!!';
+      // } else {
+      //   return i;
+      // }
+
+      return i % 4 === 0 ? i + '!!' : i;
+    }
+
+    difficultButtonArray.push(
+      <DifficultButton
+        buttonWidth={i * 20 + 100}
+        buttonParam={difficultButtonText(i)}
+        buttonColor={difficultButtonColor()}
+      />
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         {domArray}
         {bigButtonArray}
         {flexibleButtonArray}
+        {difficultButtonArray}
       </header>
     </div>
   );
