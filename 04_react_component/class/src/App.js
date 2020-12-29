2722b88f0dab9f33564c0a0260bad096ca0e3a24
KyoheiYamamoto <arienkyohei@gmail.com>
2020-12-29 23:37:15 +0900

    ReactQuest第4回講義終了

--- 04_react_component/class/src/App.js
+++ 04_react_component/class/src/App.js
@@ -1,12 +1,16 @@
-
 import './App.css';
+import RedButton from './components/training/RedButton.jsx';
 
 function App() {
-  const test = 'test param';
+  const domArray = [];
+  for (let i = 1; i <=10; i+=1) {
+     domArray.push(<RedButton param={i} />);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        {test}
+        {domArray}
+        <RedButton />
       </header>
     </div>
   );
