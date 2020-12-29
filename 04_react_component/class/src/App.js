9705ae8cb839db0d45a8606d5fdac1fad2d8e27b
KyoheiYamamoto <arienkyohei@gmail.com>
2020-12-30 00:05:01 +0900

    練習問題２完了

--- 04_react_component/class/src/App.js
+++ 04_react_component/class/src/App.js
@@ -1,16 +1,38 @@
 import './App.css';
 import RedButton from './components/training/RedButton.jsx';
+import BigButton from './components/training/BigButton.jsx';
 
 function App() {
   const domArray = [];
-  for (let i = 1; i <=10; i+=1) {
-     domArray.push(<RedButton param={i} />);
+  for (let i = 1; i <= 10; i++) {
+    domArray.push(<RedButton buttonParam={i} />);
   }
+
+  const bigButtonArray = [];
+  const colorArray = ['red', 'blue', 'yellow', 'grey', 'purple'];
+  for (let i = 1; i <= 5; i++) {
+    bigButtonArray.push(
+      <BigButton
+        buttonWidth="200px"
+        buttonParam={'test'}
+        buttonColor={colorArray[i - 1]} />
+    );
+  }
+
+  const FlexibleButtonArray = [];
+  for (let i = 1; i <= 5; i++) {
+    FlexibleButtonArray.push(
+      <FlexibleButton
+        buttonWidth={i * 20}
+      />
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         {domArray}
-        <RedButton />
+        {bigButtonArray}
       </header>
     </div>
   );
