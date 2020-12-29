b083f6e81ccf39f03c166e667e21ebf156b57865
Nakazawa Natsumi <natsumin83@icloud.com>
2020-12-29 22:46:14 +0900

    練習問題03

--- 04_react_component/class/src/App.js
+++ 04_react_component/class/src/App.js
@@ -2,28 +2,40 @@
 import './App.css';
 import Work01 from './components/training/Work01';
 import BigButton from './components/training/BigButton';
+import FlexibleButton from './components/training/FlexibleButton';
 
 function App() {
+  // 問題01
   const domArray = [];
   for (let i = 1; i <= 10; i++) {
     domArray.push(<Work01 buttonParam={i} />);
   }
+
+  // 問題02
   const bigButtonArray = [];
   const colorArray = ['red', 'blue', 'yellow', 'grey', 'purple'];
   for (let i = 1; i <= 5; i++) {
     bigButtonArray.push(
       <BigButton
         buttonWidth="200px"
-        buttonParam={'test'}
+        buttonParam="test"
         buttonColor={colorArray[i - 1]}
       />
     );
   }
+
+  // 問題03
+  const flexibleButtonArray = [];
+  for (let i = 1; i <= 5; i++) {
+    flexibleButtonArray.push(<FlexibleButton buttonWidth={i * 20 + 100} />);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         {domArray}
         {bigButtonArray}
+        {flexibleButtonArray}
       </header>
     </div>
   );
---
+++ 04_react_component/class/src/components/training/FlexibleButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class FlexibleButton extends React.Component {
+  render() {
+    return (
+      <button
+        style={{
+          width: this.props.buttonWidth,
+          height: '100px',
+          backgroundColor: 'red',
+        }}
+      >
+        test
+      </button>
+    );
+  }
+}
+
+export default FlexibleButton;
