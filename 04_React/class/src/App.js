cc1aa026c3a2b0e33a80e020c37c9cae9e9d1d9f
sedoriProg <sedori.prog@gmail.com>
2021-01-01 12:15:56 +0900

    講義中に作成したコード

--- 04_React/class/src/App.js
+++ 04_React/class/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import RedButton from "./components/training/RedButton"
 
 function App() {
-  const test = "test Param";
+  //const test = "test Param";
   return (
     <div className="App">
       <header className="App-header">
-        {test}
+      <RedButton buttonParam={"aaaa"} />
+      <RedButton buttonParam={"bbbb"} />
+      <RedButton buttonParam={"cccc"} />
       </header>
     </div>
   );
