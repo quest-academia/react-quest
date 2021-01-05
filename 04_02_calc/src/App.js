3649ebadf0b1176b5a44e52aa7365558922f49bd
ShiotaYoshiaki <shiota.yosiaki.0904@gmail.com>
2021-01-06 00:10:43 +0900

    4-3 inputタグ追加・レイアウト追加

--- 04_02_calc/src/App.js
+++ 04_02_calc/src/App.js
@@ -1,33 +1,41 @@
 import "./App.css";
 import Button from "./components/button";
+import css from './styles/layout.module.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <Button buttonParam={7} />
-          <Button buttonParam={8} />
-          <Button buttonParam={9} />
-          <Button buttonParam={'/'} />
-        </div>
-        <div>
-          <Button buttonParam={4} />
-          <Button buttonParam={5} />
-          <Button buttonParam={6} />
-          <Button buttonParam={'x'} />
-        </div>
-        <div>
-          <Button buttonParam={1} />
-          <Button buttonParam={2} />
-          <Button buttonParam={3} />
-          <Button buttonParam={'-'} />
-        </div>
-        <div>
-          <Button buttonParam={'00'} />
-          <Button buttonParam={0} />
-          <Button buttonParam={'='} />
-          <Button buttonParam={'+'} />
+        <div className={css.l_calc}>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            <div>
+              <Button buttonParam={7} />
+              <Button buttonParam={8} />
+              <Button buttonParam={9} />
+              <Button buttonParam={"/"} />
+            </div>
+            <div>
+              <Button buttonParam={4} />
+              <Button buttonParam={5} />
+              <Button buttonParam={6} />
+              <Button buttonParam={"x"} />
+            </div>
+            <div>
+              <Button buttonParam={1} />
+              <Button buttonParam={2} />
+              <Button buttonParam={3} />
+              <Button buttonParam={"-"} />
+            </div>
+            <div>
+              <Button buttonParam={"00"} />
+              <Button buttonParam={0} />
+              <Button buttonParam={"="} />
+              <Button buttonParam={"+"} />
+            </div>
+          </div>
         </div>
       </header>
     </div>
--- 04_02_calc/src/components/button.jsx
+++ 04_02_calc/src/components/button.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import css from '../styles/layout.module.css';
 
 class Button extends React.Component {
   render() {
     return (
-    <button>{this.props.buttonParam}</button>
+    <button className={css.l_button}>{this.props.buttonParam}</button>
     );
   }
 }
