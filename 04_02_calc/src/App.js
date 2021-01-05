28a1f39cba275df442c14d634799baa56c01452e
ShiotaYoshiaki <shiota.yosiaki.0904@gmail.com>
2021-01-05 23:58:31 +0900

    4-2: ボタンのレイアウト作成

--- 04_02_calc/src/App.js
+++ 04_02_calc/src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
+import Button from "./components/button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>test</div>
+        <div>
+          <Button buttonParam={7} />
+          <Button buttonParam={8} />
+          <Button buttonParam={9} />
+          <Button buttonParam={'/'} />
+        </div>
+        <div>
+          <Button buttonParam={4} />
+          <Button buttonParam={5} />
+          <Button buttonParam={6} />
+          <Button buttonParam={'x'} />
+        </div>
+        <div>
+          <Button buttonParam={1} />
+          <Button buttonParam={2} />
+          <Button buttonParam={3} />
+          <Button buttonParam={'-'} />
+        </div>
+        <div>
+          <Button buttonParam={'00'} />
+          <Button buttonParam={0} />
+          <Button buttonParam={'='} />
+          <Button buttonParam={'+'} />
+        </div>
       </header>
     </div>
   );
