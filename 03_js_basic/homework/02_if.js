8b5f3bf4c74ab1c4eefb8872d63c1f1531c6e628
masaki <keitomasaki@icloud.com>
2021-01-09 16:18:58 +0900

    宿題保存

--- 03_js_basic/homework/02_if.js
+++ 03_js_basic/homework/02_if.js
@@ -3,10 +3,10 @@
  * 正当はありません
  */
 const test1 = true;
-if (test1) {
-
+if (test1 === false) {
+    console.log('true');
 } else {
-
+    console.log('false');
 }
 
 /**
@@ -15,7 +15,7 @@ if (test1) {
 
 /**
  * 問2. 回答欄
- * 
+ * 論理演算子は変数や定数を評価して、その結果をtrueかfalseで返します
  * 
  */
 
@@ -23,7 +23,7 @@ if (test1) {
  * 問3. 論理演算子を使って、test3のconsoleでbase paramを表示させてください。
  * 変数baseの値を変更して回答してください
  */
-const base = 'param';
+const base = false;
 const test3 = base || 'base param';
 console.log(test3);
 
@@ -33,15 +33,15 @@ console.log(test3);
 
 /**
  * 問4. 回答欄
- * 
- * 
+ * 条件式 ? 式1 : 式2
+ * 三項演算子三項演算子とは、条件式には論理値や論理値に変換可能な値を記述し、条件式の値が true だった場合に式1を評価した結果を返し、 false だった場合に式2を評価した結果を返す。
  */
 
 /**
  * 問5. 三項演算子を使って、test5のconsoleでright paramを表示させてください。
  * 変数baseの値を変更して回答してください
  */
-const base = 'param';
+const base = false;
 const test5 = base ? 'left param' : 'right param';
 console.log(test5);
 
@@ -53,7 +53,7 @@ console.log(test5);
 
 /**
  * 問6. 回答欄
- * 
+ * if文とは、指定した条件式が成立した場合や、成立しなかった場合によって実行する処理を分岐したり、指定した条件式の値によって処理を分岐する。
  * 
  * 
  */
