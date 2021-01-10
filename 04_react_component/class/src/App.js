656382cad7932cb1940a81c1223223c6f3c450c2
masanori0117 <masanori.takahashi.0117@gmail.com>
2021-01-10 18:11:05 +0900

    練習3計算修正& 練習問題4

--- 04_react_component/class/src/App.js
+++ 04_react_component/class/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import RedButton from './components/training/RedButton';
 import BigButton from './components/training/BigButton';
 import FlexibleButton from './components/training/FlexibleButton';
+import DifficultButton from './components/training/DifficultButton';
+
 
 function App() {
   // 練習1 処理
@@ -25,11 +27,57 @@ function App() {
 
   // 練習3 処理
   const domFlexibleButtonArray = [];
-  for (let i=20; i<=100; i+= 20) {
+  for (let i=1; i<=5; i+= 1) {
+    let width = i*20;
     domFlexibleButtonArray.push(
       <FlexibleButton
-        flexibleWidth={i}
-        text={i+'px'}
+        flexibleWidth={width}
+        text={width+'px'}
+      />
+    )
+  }
+
+  // 練習4 処理
+  const domDifficultButtonArray = [];
+
+  for (let i=1; i<=13; i+= 1) {
+    // 横幅変更処理
+    let changeWidth = i*20;
+
+    // 配色処理
+    const changeColor = () => {
+      if (i % 3 === 0) {
+
+        // 3の倍数の場合赤色
+        return 'red';
+
+      } else {
+
+        // それ以外の場合は青色
+        return 'blue';
+      }
+    }
+
+    // 表示処理
+    const changeText = () => {
+      if (i % 4 === 0) {
+
+        // 4で割り切れる場合は !! を表示
+        return '!!'+' (' + i +')';
+
+      } else {
+
+        // それ以外は数字を表示
+        return i;
+      }
+    }
+
+    // ボタン生成処理
+    domDifficultButtonArray.push(
+      <DifficultButton
+        color={changeColor()}
+        text={changeText()}
+        width={changeWidth}
       />
     )
   }
@@ -45,6 +93,9 @@ function App() {
 
         {/* 練習3 出力 */}
         {domFlexibleButtonArray}
+
+        {/* 練習4 出力 */}
+        {domDifficultButtonArray}
 
       </header>
     </div>
