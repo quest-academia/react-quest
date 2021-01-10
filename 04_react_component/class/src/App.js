7d0a8080ac2ebb65f4c9653f73c7bd8739de8c86
masanori0117 <masanori.takahashi.0117@gmail.com>
2021-01-10 14:04:58 +0900

    練習問題1

--- 04_react_component/class/src/App.js
+++ 04_react_component/class/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import RedButton from './components/training/RedButton'
 
 function App() {
-  const test = 'testParam';
+  // const test = 'testParam';
+  const domArray = [];
+  for (let i=0; i <= 10; i +=1) {
+    domArray.push(<RedButton buttonParam={i}/>);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        { test }
+        {domArray}
       </header>
     </div>
   );
