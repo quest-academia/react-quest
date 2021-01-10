ea7d093f04eebd26323edb2932de43808c6e7a0f
tanakayuu <tanaka07yuu23@gmail.com>
2021-01-10 14:13:17 +0900

    Add: 練習問題１

--- 04_react_component/class/src/App.js
+++ 04_react_component/class/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import RedButton from './components/RedButton'
 
 function App() {
+  const domArray = [];
+  for (let i = 1; i <= 10; i += 1) {
+    domArray.push(<RedButton buttonParam={i}/>)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {domArray}
       </header>
     </div>
   );
