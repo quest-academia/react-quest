d2e0992c91ecd701a79ef2afaf352ce4e60081a6
satou-yosiyuki <stuysyk1027@icloud.com>
2021-04-29 00:05:20 +0900

    init react component

---
+++ 04_react-component/class/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  const test = 'test param';
+  return (
+    <div className="App">
+      <header className="App-header">
+      {test}
+      </header>
+    </div>
+  );
+}
+
+export default App;
